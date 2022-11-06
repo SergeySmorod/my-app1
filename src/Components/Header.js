4f1db1a43a58cec4330199f285a9db4f1e8fd4e0
Sergey <sa1974@yandex.ru>
2022-11-06 23:57:24 +0300

    14. Уроки React JS (css-модули, css-modules)

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,8 @@
 import React  from "react";
-
+import s from "./Header.module.css";
 const Header =()=>{
     return(
-        <header className='header'>
+        <header className={s.header}>
             <img src='https://avatars.mds.yandex.net/i?id=ef763c9b4e87e034dab95fc73df3e7c4028de899-5683625-images-thumbs&n=13'/>
         </header>
     );
--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,11 +1,13 @@
 import React  from "react";
+import s from "./Profile.module.css";
+
 const Profile = () => {
   return (
-          <div className='content'>
-              <div>
+          <div className={s.content}>
+              <div className={s.item}>
               <img src='https://cf1-static.cybo.com/wikiphotos_2016_05_10_13_16_21/e9/ad/e9ad4922b28fe4974d4b5b8d6ba6ffd088f5d52f/6517429_979fadec3497df0d61079db29519b4664be0d89b_CalesFonts01.jpeg'/>
               </div>
-              <div>
+              <div >
                   ava+description
               </div>
               <div>
@@ -14,10 +16,10 @@ const Profile = () => {
               <div>
                   New post
               </div>
-              <div>
+              <div className={s.item}>
                   Post 1
               </div>
-              <div>
+              <div className={s.item}>
                   Post 2
               </div>
           </div>
